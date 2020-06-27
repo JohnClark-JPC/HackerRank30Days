b0e1e2f4adb80d04020ab400d06efb95973feca1
jpc-sharp <63085653+jpc-sharp@users.noreply.github.com>
2020-06-27 18:02:01 -0400

    Create Program.cs

---
+++ 04-ClassInheritance/Program.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        //public int age = 4;
+
+
+        public Person(int initialAge)
+        {
+            // Add some more code to run some checks on initialAge
+            this.age = initialAge;
+            if (age > 0)
+            {
+                age = initialAge;
+            }
+            else
+            {
+                age = 0;
+                Console.WriteLine("Age is not valid, setting age to 0.");
+            }
+        }
+
+        public void amIOld()
+        {
+            // Do some computations in here and print out the correct statement to the console 
+
+            if (age < 13)
+            {
+                Console.WriteLine("You are young.");
+            }
+            else if (age >= 13 && age < 18)
+            {
+                Console.WriteLine("You are a teenager.");
+            }
+            else
+            {
+                Console.WriteLine("You are old.");
+            }
+        }
+ 
+        public void yearPasses()
+            // Increment the age of the person in here
+        {
+            age++;
+        }
+    }
+}
+
