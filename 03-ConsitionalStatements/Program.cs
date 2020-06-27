02c5ed370e1b3d2b801a5c9e67fe17dae5e77f52
jpc-sharp <63085653+jpc-sharp@users.noreply.github.com>
2020-06-27 17:46:36 -0400

    Create Program.cs

---
+++ 03-ConsitionalStatements/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hrConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int N = Int32.Parse(Console.ReadLine());
+           
+            if(N % 2 == 1 || N > 5 && N < 21 && N % 2 == 0) 
+            {
+                Console.WriteLine("Weird");
+            }
+            else
+            {
+                Console.WriteLine("Not Weird");
+            }
+            Console.ReadLine();
+        }
+    }
+}
