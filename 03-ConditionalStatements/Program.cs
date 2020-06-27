37b266746d7d4e7f782c010cffe64db8498592d0
jpc-sharp <63085653+jpc-sharp@users.noreply.github.com>
2020-06-27 17:47:32 -0400

    Update and rename 03-ConsitionalStatements/Program.cs to 03-ConditionalStatements/Program.cs

--- 03-ConditionalStatements/Program.cs
+++ 03-ConditionalStatements/Program.cs
@@ -10,8 +10,8 @@ namespace hrConditionalStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number: ");
-            int N = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter a number: ");
+            //int N = Int32.Parse(Console.ReadLine());
            
             if(N % 2 == 1 || N > 5 && N < 21 && N % 2 == 0) 
             {
@@ -21,7 +21,7 @@ namespace hrConditionalStatements
             {
                 Console.WriteLine("Not Weird");
             }
-            Console.ReadLine();
+            //Console.ReadLine();
         }
     }
 }
