864eae89dd757e54ec8cd68d3a8e225b8a482d24
jpc-sharp <63085653+jpc-sharp@users.noreply.github.com>
2020-06-28 14:41:31 -0400

    Create Program.cs

---
+++ 07-Arrays/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = (n - 1); i >= 0 ; i--)
+            {
+                Console.Write("{0}" + " ", arr[i]);
+            }
+        }
+    }
+}
